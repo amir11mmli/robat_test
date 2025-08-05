e5020dacaf5a5ae73aea3d706e5c07a8091b7cc9
amir11mmli <amir.mmli.or@gmail.com>
2025-08-05 22:51:09 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from aiogram.dispatcher.filters import CommandStart
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+PHOTO_FILE_ID = os.getenv("PHOTO_FILE_ID")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(CommandStart())
+async def start(message: types.Message):
+    user_id = message.from_user.id
+    member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
+
+    if member.status != "left":
+        sent_msg = await message.answer_photo(PHOTO_FILE_ID)
+        await asyncio.sleep(30)
+        try:
+            await bot.delete_message(chat_id=message.chat.id, message_id=sent_msg.message_id)
+        except:
+            pass
+    else:
+        await message.reply("❌ لطفاً اول عضو کانال بشو تا بتونم ادامه بدم.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
